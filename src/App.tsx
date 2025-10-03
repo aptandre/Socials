096deef281a27f51eff76a5a022dcb66185f6316
andre <andre.alves@ccc.ufcg.edu.br>
2025-10-03 11:10:01 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{
+        backgroundColor: 'blue',
+        width: '200px',
+        height: '200px'
+      }}>
+      </div>
+    </>
+  )
+}
+
+export default App
